51200656f5525c1377ee4bf06e83541cb62d6a74
murphyslaw480 <murphyslaw480@gmail.com>
2013-09-09 22:33:50 -0400

    work on equip ui

---
+++ SpaceGame/ui/UIElement.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace SpaceGame.ui
+{
+    class UIElement
+    {
+        Texture2D _texture;
+
+    }
+}
